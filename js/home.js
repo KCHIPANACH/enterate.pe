3528f7eb97ac1416b7e4da8319954ff412d64671
Kevin Chipana <kvn.chipana@gmail.com>
2020-12-03 22:55:43 -0500

    validaciones y deslike psd jeyson se la come doblada

--- js/home.js
+++ js/home.js
@@ -1,25 +1,44 @@
 
-
+const hasClass = (el, className) => el.classList.contains(className);
 
 function handleLikePost({target},id_post) {
+    
+    if(hasClass(target.parentElement, "active")){
+      target.parentElement.classList.toggle("active")
+
+      let array_likes = Array.from(JSON.parse(localStorage.likePost));
+
+      array_likes.map((item,index)=>
+         (item == id_post) &&
+            array_likes.splice(index,1)
+      )
+      console.log(array_likes);
+      localStorage.setItem("likePost", JSON.stringify(array_likes))
+
+    }else{
+      target.parentElement.classList.toggle("active")
+ 
+      if (!localStorage.getItem("likePost")) {
    
-    target.parentElement.classList.toggle("active")
- 
-    if (!localStorage.getItem("likePost")) {
- 
-       localStorage.setItem("likePost", `[${id_post}]`)
- 
-       console.log(localStorage.likePost)
-    }
-    else{
- 
-       let array_likes = Array.from(JSON.parse(localStorage.likePost))
-       array_likes.push(Number(id_post))
-       
-       localStorage.setItem("likePost", JSON.stringify(array_likes))
- 
-       console.log(localStorage.likePost)
-       
+         localStorage.setItem("likePost", `[${id_post}]`)
+   
+         console.log(localStorage.likePost)
+      }
+      else{
+   
+         let array_likes = Array.from(JSON.parse(localStorage.likePost))
+         if(array_likes.length > 0){
+            if(array_likes.indexOf(Number(id_post)) == -1 ){
+               array_likes.push(Number(id_post))
+            }
+         }
+         
+         localStorage.setItem("likePost", JSON.stringify(array_likes))
+
+   
+         console.log(localStorage.likePost)
+         
+      }
     }
  }
 
